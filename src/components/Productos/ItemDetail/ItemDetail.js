9bd32950be4ab68a504b2a051ed47476657e2ac2
emipedraza <j.p.e.23.11.98@gmail.com>
2022-10-05 06:19:08 -0300

    style-based-details

--- src/components/Productos/ItemDetail/ItemDetail.js
+++ src/components/Productos/ItemDetail/ItemDetail.js
@@ -1,43 +1,47 @@
-import {useParams} from 'react-router-dom'
-import ItemCount from '../ItemCount/ItemCount';
-import {useState, useContext, useEffect} from 'react';
-import { CartContext } from '../../../context/CartContext';
-import {doc, getDoc} from "firebase/firestore"
-import { db } from '../../../utils/fireBase';
+import { useParams } from "react-router-dom";
+import ItemCount from "../ItemCount/ItemCount";
+import { useState, useContext, useEffect } from "react";
+import { CartContext } from "../../../context/CartContext";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../../utils/fireBase";
 
-const ItemDetail = ()=>{
-  const {id} = useParams() 
-  const [product, setProduct] = useState({})
+const ItemDetail = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
 
+  const { addItem } = useContext(CartContext);
 
-  const {addItem} = useContext(CartContext)
+  useEffect(() => {
+    const getProd = async () => {
+      const item = doc(db, "items", id);
+      const prod = await getDoc(item);
+      const newPrd = { ...prod.data(), id: prod.id };
+      setProduct(newPrd);
+    };
+    getProd();
+  }, []);
 
-  useEffect(()=>{
-    const getProd = async()=>{
-      const item = doc(db, "items", id)
-      const prod = await getDoc(item)
-      const newPrd = {...prod.data(),id: prod.id}
-      setProduct(newPrd)
-    }
-    getProd()
-  },[])
+  const onAdd = (dat) => {
+    addItem(product, dat);
+  };
 
+  return (
+    <div className="--totalData">
+      <div className="--divImgBox">
+        <img src={product.image} className="--imgPrd" />
+      </div>
+      <div className="--divDataBox">
+        <h3 className="--tittlePrd-">{product.name}</h3>
+        <ul className="--datPrdA-">
+          <li className="--categ-">categoria: {product.categoria}</li>
+          <li className="--price-">Precio: {product.price}$</li>
+          <li className="--stock-">Stock: {product.stock}</li>
+        </ul>
+        <p className="--especificacines">{product.detail}</p>
+        <ItemCount onAdd={onAdd} />
+      </div>
+    </div>
+  );
+};
 
-  const onAdd =(dat)=>{
-    addItem(product, dat)
-  }
-  
-  return(<div className="datos">
-                  <img src={product.image} className="--imgPrd" />
-                  <h3 className="--tittlePrd-">{product.name}</h3>
-                  <ul className="--datPrdA-">
-                    <li className="--categ-">categoria: {product.categoria}</li>
-                    <li className="--price-">Precio: {product.price}$</li>
-                    <li className="--stock-">Stock: {product.stock}</li>
-                  </ul>
-                  <p className="--especificacines">{product.detail}</p>
-                  <ItemCount onAdd={onAdd}/>
-          </div>) 
-}
-
-export default ItemDetail 
+export default ItemDetail;
